13a08abbb48898e013cce708c8529231ecaa3284
Anderson <daosantiago@gmail.com>
2022-07-12 19:57:10 +0100

    Add first configs to dev environment

---
+++ Nivel-1/frontend/babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env', // Converte a versão do JS de acordo com o ambiente
+    '@babel/preset-react', // Converte as funcionalidades do React para o Browser
+  ],
+}
---
+++ Nivel-1/frontend/src/index.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { render } from 'react-dom';
+
+const soma = (a, b) => {
+  return a + b;
+}
+
+console.log(soma(5, 3));
+
+//render(<h1>Hello</h1>, document.getElementById('app'))
