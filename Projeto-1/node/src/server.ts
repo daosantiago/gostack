b3f5a7641ddcf019a3e428891efb77b94740c0b1
Anderson <daosantiago@gmail.com>
2022-07-17 11:36:29 +0100

    debugger config

--- Projeto-1/node/src/server.ts
+++ Projeto-1/node/src/server.ts
@@ -3,11 +3,9 @@ import express, { application } from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-  console.log('chegou');
-
   return response.json({message: 'Hello'});
 });
 
-application.listen(3333, () => {
+app.listen(3333, () => {
   console.log('🚀 Server is listening on http://localhost:3333');
 });
