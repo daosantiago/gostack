3150a6c615e27285010197d74a218127b0d67760
Anderson <daosantiago@gmail.com>
2022-07-17 19:57:00 +0100

    final configs

--- Projeto-1/node/src/server.ts
+++ Projeto-1/node/src/server.ts
@@ -1,10 +1,10 @@
-import express, { application } from 'express';
+import express from 'express';
+import routes from './routes'
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello'});
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Server is listening on http://localhost:3333');
