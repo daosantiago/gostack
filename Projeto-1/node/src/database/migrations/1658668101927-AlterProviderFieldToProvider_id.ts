3971bd3d678c34459e2148d7312a08b0ae0dd9a7
Anderson <daosantiago@gmail.com>
2022-07-24 14:41:41 +0100

    alter provider on appointments

---
+++ Projeto-1/node/src/database/migrations/1658668101927-AlterProviderFieldToProvider_id.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, Table, TableColumn, TableForeignKey } from "typeorm";
+
+export default class AlterProviderFieldToProviderId1658668101927 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('appointments', 'provider');
+
+    await queryRunner.addColumn('appointments',
+      new TableColumn({
+        name: 'provider_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await await queryRunner.createForeignKey('appointments', new TableForeignKey({
+      name: 'AppointmentProviderFQ',
+      columnNames: ['provider_id'],
+      referencedColumnNames: ['id'],
+      referencedTableName: 'users',
+      onDelete: 'SET NULL',
+      onUpdate: 'CASCADE',
+    }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('appointments', 'FQAppointmentProvider');
+
+    await queryRunner.dropColumn('appointments', 'provider_id');
+
+    await queryRunner.addColumn('appointments', new TableColumn({
+      name: 'provider',
+      type: 'varchar',
+    }));
+  }
+
+}
