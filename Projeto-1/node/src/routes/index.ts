d8ee6b30e2fc32144f3af2564c42c4803c850641
Anderson <daosantiago@gmail.com>
2022-07-24 15:04:35 +0100

    add user to routes

--- Projeto-1/node/src/routes/index.ts
+++ Projeto-1/node/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import appointmentsRouter from "./appointments.routes";
+import usersRouter from "./users.routes";
 
 const routes = Router();
 
 // Redireciona as rotas de '/appointments' para appointementsRouter
 routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
