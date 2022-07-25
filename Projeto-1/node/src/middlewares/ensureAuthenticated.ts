9b54884bfc0c6078f7f53663a853580fc8b9509f
Anderson <daosantiago@gmail.com>
2022-07-25 20:38:52 +0100

    add enrureAuthenticated

---
+++ Projeto-1/node/src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,42 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error('JWT token is missing');
+  }
+
+  // Desestruturação com colchetes, pois não retorna um objeto e sim uma lista
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decoded as TokenPayload;
+
+    // Para ser aceito o termo request.user foi feito uma sobrecarda de tipos do Request no arquivo @types/express.d.ts
+    request.user = {
+      id: sub,
+    };
+
+    // next é a próxima função a ser chamada caso tudo dê certo no middleware.
+    // Essa função deve ser enviada como parâmetro quando o middleware for chamado.
+    return next();
+  } catch (error) {
+    throw new Error('Invalid JWT token');
+  }
+}
