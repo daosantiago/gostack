139f10f6e5ee76a82476c308731a48378ba4904b
Anderson <daosantiago@gmail.com>
2022-07-25 23:25:25 +0100

    add handle to avatar route

--- Projeto-1/node/src/routes/users.routes.ts
+++ Projeto-1/node/src/routes/users.routes.ts
@@ -5,6 +5,7 @@ import uploadConfig from '../config/upload';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -32,9 +33,19 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
 
-    return response.json({ ok: true });
+      delete user.password;
+
+      return response.json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   },
 );
 
