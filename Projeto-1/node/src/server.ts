0a2e35083a1e090e5c5bef0b3fed6e855c7d48de
Anderson <daosantiago@gmail.com>
2022-07-25 23:29:31 +0100

    add route to show static files

--- Projeto-1/node/src/server.ts
+++ Projeto-1/node/src/server.ts
@@ -1,13 +1,15 @@
 import 'reflect-metadata';
 
 import express from 'express';
-import routes from './routes'
+import routes from './routes';
+import uploadCondig from './config/upload';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadCondig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
