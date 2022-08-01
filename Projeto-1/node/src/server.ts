f4cd7a3f1c269e47b4ebb7526cc3040d6b3db3a5
Anderson <daosantiago@gmail.com>
2022-08-01 10:41:48 +0100

    add error handling to main server.ts

--- Projeto-1/node/src/server.ts
+++ Projeto-1/node/src/server.ts
@@ -1,8 +1,13 @@
 import 'reflect-metadata';
 
 import express from 'express';
+import { NextFunction, Request, Response } from 'express';
+// Necessário instalar express-async-errors para tratativas de erros assincronos para versão do express
+import 'express-async-errors';
+
 import routes from './routes';
 import uploadCondig from './config/upload';
+import AppError from './errors/AppError';
 
 import './database';
 
@@ -11,6 +16,24 @@ const app = express();
 app.use(express.json());
 app.use('/files', express.static(uploadCondig.directory));
 app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log('');
