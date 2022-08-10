f317ca8cf59233bc7d3dda916dcc45f95977ec69
Anderson <daosantiago@gmail.com>
2022-08-10 23:32:19 +0100

    create form and repositories list for dashboard

--- Nivel-3/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ Nivel-3/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,8 +1,59 @@
 import React from "react";
+import { FiChevronRight } from "react-icons/fi";
 
-import { Title } from "./styles";
+import logoImg from "../../assets/logo.svg";
+
+import { Title, Form, Repositories } from "./styles";
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore Repositórios do Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer logo" />
+      <Title>Explore Repositórios do Github</Title>
+      <Form>
+        <input type="text" placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="#">
+          <img
+            src="https://avatars.githubusercontent.com/u/25793311?v=4"
+            alt="Anderson Santiago"
+          />
+          <div>
+            <strong>Casteljau</strong>
+            <p>Curva de Casteljau</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="#">
+          <img
+            src="https://avatars.githubusercontent.com/u/25793311?v=4"
+            alt="Anderson Santiago"
+          />
+          <div>
+            <strong>Casteljau</strong>
+            <p>Curva de Casteljau</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="#">
+          <img
+            src="https://avatars.githubusercontent.com/u/25793311?v=4"
+            alt="Anderson Santiago"
+          />
+          <div>
+            <strong>Casteljau</strong>
+            <p>Curva de Casteljau</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 export default Dashboard;
