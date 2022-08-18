8ccc76a9272b2b165a14dd113855c59dc07257c2
Anderson <daosantiago@gmail.com>
2022-08-18 23:22:47 +0100

    add repository detail sctructure

--- Nivel-3/05-primeiro-projeto-react/src/pages/Repository/index.tsx
+++ Nivel-3/05-primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,12 +1,65 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 interface RepositoryParams {
   repository: string;
 }
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository: {params.repository}</h1>;
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Githb Explorer" />
+        <Link to="/dashboard">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars.githubusercontent.com/u/25793311?v=4"
+            alt="Foto de perfil"
+          />
+          <div>
+            <strong>usuário/repositório</strong>
+            <p>Descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1101</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to={`/repository/`}>
+          <div>
+            <strong>repository.full_name</strong>
+            <p>repository.description</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 export default Repository;
