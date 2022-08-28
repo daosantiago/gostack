0ab75ed317e84f77fd94dd288ce22f44f80b400b
Anderson <daosantiago@gmail.com>
2022-08-28 13:30:50 +0100

    create signUp page

--- 06-gobarber-web/src/pages/SignUp/index.tsx
+++ 06-gobarber-web/src/pages/SignUp/index.tsx
@@ -1,43 +1,43 @@
 import React from 'react';
-import {
-  FiArrowDownLeft,
-  FiLogIn,
-  FiMail,
-  FiLock,
-  FiUser,
-} from 'react-icons/fi';
+import { FiArrowDownLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 
+import { Form } from '@unform/web';
 import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.svg';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-Mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
 
-      <form>
-        <h1>Faça seu cadastro</h1>
-        <Input name="name" icon={FiUser} placeholder="Nome" />
-        <Input name="email" icon={FiMail} placeholder="E-Mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Senha"
-        />
+          <Button type="submit">Cadastrar</Button>
+        </Form>
 
-        <Button>Cadastrar</Button>
-      </form>
+        <a href="criar">
+          <FiArrowDownLeft />
+          Voltar para Logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
-      <a href="criar">
-        <FiArrowDownLeft />
-        Voltar para Logon
-      </a>
-    </Content>
-    <Background />
-  </Container>
-);
 export default SignUp;
