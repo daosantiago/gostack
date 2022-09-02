efc0a7f28ca309581093c342484a6bc11e75fe2d
Anderson <daosantiago@gmail.com>
2022-09-02 21:34:24 +0100

    create routes

--- 06-gobarber-web/src/routes/index.tsx
+++ 06-gobarber-web/src/routes/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import Dashboard from "./pages/Dashboard";
-import Repository from "./pages/Repository";
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
 
 const Routes: React.FC = () => (
   // Switch garante que apenas uma rota seja exibida
   <Switch>
-    <Route path="/" exact component={Dashboard} />
-    <Route path="/repository" component={Repository} />
+    <Route path="/" exact component={SignIn} />
+    <Route path="/signup" component={SignUp} />
   </Switch>
 );
 
