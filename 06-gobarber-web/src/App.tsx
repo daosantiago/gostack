1569e9b27bfa39933c44804d490b90a43a660ec1
Anderson <daosantiago@gmail.com>
2022-09-03 10:25:55 +0100

    create a Route extension

--- 06-gobarber-web/src/App.tsx
+++ 06-gobarber-web/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
-import SignIn from './pages/SignIn';
 
 import AppProvider from './hooks';
 
