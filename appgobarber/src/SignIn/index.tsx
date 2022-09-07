1117ebb63aab98db87708578a547c0bea06fe1d9
Anderson <daosantiago@gmail.com>
2022-09-07 19:58:00 +0100

    create signIn and SignUp pages

---
+++ appgobarber/src/SignIn/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Container } from './styles';
+import { View, Text, StatusBar } from 'react-native';
+
+const SignIn: React.FC = () => {
+  return <Container />;
+};
+
+export default SignIn;
