14ae593d9c9015e53680de81c78739e0c0e35e99
Anderson <daosantiago@gmail.com>
2022-09-07 19:59:32 +0100

    create routes

--- appgobarber/src/App.tsx
+++ appgobarber/src/App.tsx
@@ -1,9 +1,17 @@
-import React from "react";
-import { View, Text } from "react-native";
+import 'react-native-gesture-handler';
+
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
 
 const App: React.FC = () => (
-  <View>
-    <Text>Minha nova App</Text>
-  </View>
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+      <Routes />
+    </View>
+  </NavigationContainer>
 );
 export default App;
