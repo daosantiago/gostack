21d73311431335590a78662ca279a8717e2c55e4
Anderson <daosantiago@gmail.com>
2023-02-25 17:19:26 +0000

    Error introduced when ref AppointmentsRepository

--- Projeto-1/node/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ Projeto-1/node/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,21 +1,26 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 
 import Appointment from '../entities/Appointment';
 
-@EntityRepository(Appointment)
-class AppointmentsRepository
-  extends Repository<Appointment>
-  implements IAppointmentsRepository
-{
+class AppointmentsRepository implements IAppointmentsRepository {
   // private appointments: Appointment[] = [];
+
+  private ormRepository: Repository<Appointment>;
+
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
+
+  // Durante a aula "Reescrevendo repositórios" este repositório começou a quebrar a aplicação
+  // https://app.rocketseat.com.br/node/nivel-04/group/arquitetura-e-ddd/lesson/reescrevendo-repositorios
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
     // const findAppointment = this.appointments.find(appointment =>
     //   isEqual(date, appointment.date),
     // );
-    const findAppointment = await this.findOne({
+    const findAppointment = await this.ormRepository.findOne({
       where: { date },
     });
 
