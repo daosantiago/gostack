3e601477656f09eebff34d82098d84bb84e3920f
Anderson <daosantiago@gmail.com>
2023-02-25 19:45:37 +0000

    finish what was unfinished due the prev error

--- Projeto-1/node/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ Projeto-1/node/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,18 +1,29 @@
+/* eslint-disable camelcase */
 import { getRepository, Repository } from 'typeorm';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import Appointment from '../entities/Appointment';
 
 class AppointmentsRepository implements IAppointmentsRepository {
   // private appointments: Appointment[] = [];
-
-  private ormRepository: Repository<Appointment>;
-
   constructor() {
     this.ormRepository = getRepository(Appointment);
   }
 
+  private ormRepository: Repository<Appointment>;
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = this.ormRepository.create({ provider_id, date });
+
+    await this.ormRepository.save(appointment);
+
+    return appointment;
+  }
   // Durante a aula "Reescrevendo repositórios" este repositório começou a quebrar a aplicação
   // https://app.rocketseat.com.br/node/nivel-04/group/arquitetura-e-ddd/lesson/reescrevendo-repositorios
 
