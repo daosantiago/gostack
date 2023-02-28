cf2aed9aab7d8f1a87e4663c93521cd8e6462d74
Anderson <daosantiago@gmail.com>
2023-02-28 21:33:42 +0000

    create test for same date appointments

--- Projeto-1/node/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ Projeto-1/node/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -16,5 +16,25 @@ describe('CreateAppointment', () => {
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('321321321');
   });
-  //it('should not be able to create two appointment in the same datetime', () => {});
+
+  it('should not be able to create two appointment in the same datetime', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2023, 2, 1, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '321321321',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '321321321',
+      }),
+    ).rejects.toBeInstanceOf(Error);
+  });
 });
